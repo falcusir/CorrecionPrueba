f347664c62d4bf317663e0c37f83aee3b0904af4
falcuir <maria.alcusir1989@ist17dejulio.edu.ec>
2024-12-08 17:06:36 -0500

    Clase Dirección

--- src/modelo/Direccion.java
+++ src/modelo/Direccion.java
@@ -9,5 +9,61 @@ package modelo;
  * @author Dell Core i7
  */
 public class Direccion {
+    //Atributos
+    private String calle;
+    private String ciudad;
+    private int codPostal;
+    private String pais;
+    //Constructores
+
+    public Direccion() {
+    }
+
+    public Direccion(String calle, String ciudad, int codPostal, String pais) {
+        this.calle = calle;
+        this.ciudad = ciudad;
+        this.codPostal = codPostal;
+        this.pais = pais;
+    }
+    //Encapsulamiento
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public int getCodPostal() {
+        return codPostal;
+    }
+
+    public void setCodPostal(int codPostal) {
+        this.codPostal = codPostal;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    //Reglas de Negocio
+
+    @Override
+    public String toString() {
+        return "DIRECCION: " + getPais()+", "+getCodPostal()+", "+ getCiudad()+", "+ getCalle();
+    }
+    
     
 }
--- src/vista/Main.java
+++ src/vista/Main.java
@@ -4,10 +4,17 @@
  */
 package vista;
 
+import modelo.Direccion;
+
 /**
  *
  * @author Dell Core i7
  */
 public class Main {
+    public static void main(String[] args) {
+        Direccion d1 = new Direccion("Tungurahua", "Ibarra", 593, "Ecuador");
+        System.out.println(d1.toString());
+    }
+    
     
 }
